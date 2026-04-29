726c05ffaeabe3961e2615f976004eb9c836b4b9
parmarupasana <126671260+parmarupasana@users.noreply.github.com>
2026-04-29 11:57:22 +0530

    Update API_URL to new server address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 
-const API_URL = 'http://localhost:5000/students';
+const API_URL = 'http://44.205.18.72:5000/students';
 
 function App() {
   const [students, setStudents] = useState([]);
